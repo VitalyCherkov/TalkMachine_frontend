c3b9f4f6b661a0903e1ac507aca28eabc1666281
Vitaly <vv-ch@bk.ru>
2018-06-25 13:14:52 +0300

    Intital settings created

---
+++ src/components/container.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+const Container = () => {
+    return <h1>Hello fff AAAA!</h1>;
+};
+
+export default Container;
---
+++ webpack.prod.js
@@ -0,0 +1,33 @@
+const publicPath = '/dist/build';
+const path = require('path');
+const webpack = require('webpack');
+
+
+module.exports = {
+    entry: './index.js',
+
+    output: {
+        path: path.join(__dirname, 'dist/assets'),
+        publicPath: publicPath,
+        sourceMapFilename: '[name].map'
+    },
+
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            mangle: {
+                screw_ie8: true,
+                keep_fnames: true
+            },
+            compress: {
+                screw_ie8: true
+            },
+            comments: false
+        })
+    ]
+};
